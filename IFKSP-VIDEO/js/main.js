bd5b148daf02de1c43c1c896cc9bd0dc3f179861
KPKate <palanki.katalin@gmail.com>
2020-06-29 19:06:53 +0200

    Update main.js

--- IFKSP-VIDEO/js/main.js
+++ IFKSP-VIDEO/js/main.js
@@ -1,5 +1,6 @@
 function calcAmount () {
-    let price = 1000;                                                                                                                       
+ let price = 1000;                                                                                                                       
  let amountInput = document.querySelector ( "input[name='amount-input']");
  let amount = parseInput(amountInput.value)*price;
+ alert ("Fizetendő :" + amount + "Ft")
 }
