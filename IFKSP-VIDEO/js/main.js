00ee05f11f43434958ec34ece53405a375a13d95
KPKate <palanki.katalin@gmail.com>
2020-06-30 19:48:32 +0200

    new file

--- IFKSP-VIDEO/js/main.js
+++ IFKSP-VIDEO/js/main.js
@@ -1,8 +1,8 @@
-function calcAmount () {
-let alapar= 1250;
-let price =parseInt( alapar);  
-let amountInput = document.querySelector ( "input[name='amount-input']");
- let showAmount = document.querySelector ( "span.show-amount");
- let amount = parseInt(amountInput.value)*price;
-  showAmount.innerHTML =amount;
+function calcAmount() {
+  let alapar = 1250;
+  let price = parseInt(alapar);
+  let amountInput = document.querySelector("input[name='amount-input']");
+  let showAmount = document.querySelector("span.show-amount");
+  let amount = parseInt(amountInput.value) * price;
+  showAmount.innerHTML = amount;
 }
