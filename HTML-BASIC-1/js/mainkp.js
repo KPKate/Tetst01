702041bba2614087c4d7d321fa627900609e2eed
KPKate <palanki.katalin@gmail.com>
2020-07-16 15:27:31 +0200

    1 és 10 közötti feltétel

--- HTML-BASIC-1/js/mainkp.js
+++ HTML-BASIC-1/js/mainkp.js
@@ -1,10 +1,23 @@
 function calckpAmount() {
     console.log ("Hello ifksp mainkp")
-      let alapar = 1250;
+      let alapar = 1250; minkp=1; maxkp=10;
       let price = parseInt(alapar);
       let amountInput = document.querySelector("input[name='amount-input']");
       let showAmount = document.querySelector("span.show-amount");
-      let amount = parseInt(amountInput.value) * price;
+      let amountNumber=parseInt(amountInput.value);
+      amountNumber=isNaN(amountNumber) ?0:amountNumber;
+      vizsgal( amountNumber);
+
+      let amount = amountNumber * price;
       showAmount.innerHTML = amount;
       console.log(amount);
+}
+
+function vizsgal(amountv) {
+let min=1; max=10; amountv; 
+if (amountv < min ) { amounte=min;
+alert ("legalább 1 terméket kell vásárolnia");}
+ else if (amountv > 10) {
+alert ("maximum 10 db terméket vásárolhat!");}
+else {} return;                              
 }
