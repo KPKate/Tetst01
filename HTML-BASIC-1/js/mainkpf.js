2c7e1597067155160bb7e3af21f1455e9a5a3e1a
KPKate <palanki.katalin@gmail.com>
2020-07-19 09:37:00 +0200

    Update mainkpf.js

--- HTML-BASIC-1/js/mainkpf.js
+++ HTML-BASIC-1/js/mainkpf.js
@@ -1,30 +1,33 @@
 function calckpfAmount() {
-    console.log ("Hello  mainkp")
-      let alapar = 1250; minkp=1; maxkp=10;
-      let price = parseInt(alapar);
-      let amountInput = document.querySelector("input[name='amount-input']");
-      let showAmount = document.querySelector("span.show-amount");
-      let amountNumber=parseInt(amountInput.value);
-      amountNumber=isNaN(amountNumber) ? 0 : amountNumber;
-      amountNumber1=amountNumber;
-     var amountNumber1= vizsgal(amountNumber);
-      console.log(amountNumber,amountNumber1);
-      amountNumber=amountNumber1*price;
-    showAmount.innerHTML=amountNumber;
-    return;
+  console.log("Hello  mainkp")
+  let alapar = 1250; minkp = 1; maxkp = 10;
+  let price = parseInt(alapar);
+  let amountInput = document.querySelector("input[name='amount-input']");
+  let showAmount = document.querySelector("span.show-amount");
+  let amountNumber = parseInt(amountInput.value);
+  amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+  amountNumber1 = amountNumber;
+  var amountNumber1 = vizsgal(amountNumber);
+  console.log(amountNumber, amountNumber1);
+  amountNumber = amountNumber1 * price;
+  showAmount.innerHTML = amountNumber;
+  return;
+}
+
+function vizsgal(amountv, amountv1) {
+  let min = 1;
+  let max = 10;
+  let price = 1250;
+  if (amountv < min) {
+    amountv = min - 1;
+    alert("legalább 1 terméket kell vásárolnia");
   }
-  
-  function vizsgal(amountv,amountv1) {
-    let min=1;
-    let max=10;
-    let price=1250;
-    
-    if (amountv < min ) { amountv=min-1;
-      alert ("legalább 1 terméket kell vásárolnia");}
-      else if (amountv > 10) {amountv=0;
-        alert ("maximum 10 db terméket vásárolhat!");}
-        else { amountv=amountv;}
-      amountv1=amountv;
-      console.log(amountv, amountv1);
-        return  amountv1;                  
+  else if (amountv > 10) {
+    amountv = 0;
+    alert("maximum 10 db terméket vásárolhat!");
+  }
+  else { amountv = amountv; }
+  amountv1 = amountv;
+  console.log(amountv, amountv1);
+  return amountv1;
 }
