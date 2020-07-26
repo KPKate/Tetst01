230c3f596f502ff202606ebdda5ca3d6c663a331
KPKate <palanki.katalin@gmail.com>
2020-07-26 17:47:15 +0200

    Update main.js

--- IFKSP-VIDEO/js/main.js
+++ IFKSP-VIDEO/js/main.js
@@ -1,17 +1,42 @@
 function calcAmount() {
-  console.log ("Hello ifksp main ??")
-//  let alapar = 1250;
-//  let mindb=1;
-//  let maxdb=10;
-  //let price = parseInt(alapar);
-  //let amountInput = document.querySelector("input[name='amount-input']");
-  //let showAmount = document.querySelector("span.show-amount");
-  //let amount = parseInt(amountInput.value);
-  //let amountRendel=rendelesszam( mindb, maxdb, amount);
-  //let amount = amountRendel * price;
-  //showAmount.innerHTML = amount;
-  //console (amount,showAmount);
+  let alapar = 1250; minkp = 1; maxkp = 10;
+  let price = parseInt(alapar);
+  let amountInput = document.querySelector("input[name='amount-input']");
+  let showAmount = document.querySelector("span.show-amount");
+  let amountNumber = parseInt(amountInput.value);
+  amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+  amountNumber1 = amountNumber;
+  var amountNumber1 = vizsgal(amountNumber);
+  console.log(amountNumber, amountNumber1);
+  amountNumber = amountNumber1 * price;
+  showAmount.innerHTML = amountNumber;
+  return;
 }
-//function  rendelesszam( minfdb, maxfdb, rendelsz) {
-//rendelsz=min
-//}
+
+function vizsgal(amountv, amountv1) {
+  let min = 1;
+  let max = 10;
+  let price = 1250;
+  if (amountv < min) {
+    amountv = min - 1;
+    alert("legalább 1 terméket kell vásárolnia");
+  }
+  else if (amountv > 10) {
+    amountv = 0;
+    alert("maximum 10 db terméket vásárolhat!");
+  }
+  else { amountv = amountv; }
+  amountv1 = amountv;
+  console.log(amountv, amountv1);
+  return amountv1;
+}
+// Add helptext
+let helpText = document.createElement("small");
+helpText.className = "form-text text-muted";
+helpText.innerHTML = "Adja meg a feltéteket!";
+//console.log ("help-text");
+let parent = document.querySelector("div.form-group:nth-child(1)");
+parent.appendChild(helpText);
+// let helpTEXT eltávolítása
+parent.removeChild(helpText);
+
