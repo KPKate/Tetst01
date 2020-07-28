fd30a74cffe4f6c62c5193bfe602749c32a32025
KPKate <palanki.katalin@gmail.com>
2020-07-29 00:40:03 +0200

    HTM Java array

---
+++ IFKSP-VIDEO/js/btkp.js
@@ -0,0 +1,22 @@
+<tr>
+<td>1</td>
+<td>Kiss</td>
+<td>Zoltán</td>
+<td>36 </td>
+<td>
+    <div class=" btn-group">
+        <button class=" btn-info btn">
+            <i class="fas fa-sync-alt"></i>
+        </button>
+        <button class=" btn-danger btn">
+            <i class=" far fa-trash-alt"></i>
+        </button>
+    </div>
+</td>
+</tr>
+
+let user =[
+    {
+
+];
+let tableBody
