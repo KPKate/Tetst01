5e0fd774373288b504b8bf02c8ab4ee632cd5841
KPKate <palanki.katalin@gmail.com>
2020-07-29 13:40:35 +0200

    táblázat sorszáma

--- IFKSP-VIDEO/js/btkp.js
+++ IFKSP-VIDEO/js/btkp.js
@@ -1,4 +1,4 @@
-<tr>
+/*<{tr>
 <td>1</td>
 <td>Kiss</td>
 <td>Zoltán</td>
@@ -13,10 +13,27 @@
         </button>
     </div>
 </td>
-</tr>
+</tr> }*/
 
-let user =[
-    {
-
+let users =[
+ {surname:"Kiss", firstname:"Zoltán", age:36},
+ {surname:"Nagy", firstname:"István", age:46},
+ {surname:"Tóth", firstname:"János", age:62},
+ {surname:"Balla", firstname:"Patri", age:3},
+ {surname:"Szabó", firstname:"Ági", age:56},
+ {surname:"Kéry", firstname:"Zatán", age:36},
+ {surname:"Váry", firstname:"Hanna", age:30},
+ {surname:"Váry", firstname:"Kinga", age:30},
+ {surname:"Vajda", firstname:"Árpád", age:54},
+ {surname:"Nagy", firstname:"Edit", age:57},
+ {surname:"Dér", firstname:"László", age:39},
 ];
-let tableBody
+let tableBody= document.querySelector("#userTable tbody");
+//let createdTD = (html,parent) => {
+for (let k in users) {
+    let tr= document.createElement("tr");
+    let td= document.createElement("td");
+    tr.innerHTML =parseInt(k)+1;
+    tr.appendChild(td);
+    tableBody.appendChild(tr);
+}
