07cda3667df4e1d2d8d01f99dcdec1551fb8f545
KPKate <palanki.katalin@gmail.com>
2020-08-06 11:26:33 +0200

    akció gombok+fontawesome

--- IFKSP-Server/js/main.js
+++ IFKSP-Server/js/main.js
@@ -34,17 +34,30 @@ document.querySelector("#getDataBtn").addEventListener("click", function() {
             td.innerHTML = row[k];
             tr.appendChild(td);
         }
+        let btnGroup = createBtnGroup();
+        tr.appendChild(btnGroup);
         tBody.appendChild(tr);
        }
-
    }
 
    function createAnyElement(name, attributes) {
         let element = document.createElement(name);
         for (let k in attributes) {
-            element.setAttributes(k,attributes[k]);
+            element.setAttribute(k,attributes[k]);
         }
         return element;
    }
 
+function createBtnGroup () {
+    let group = createAnyElement("div",{class: "btn btn-group"}); 
+     let infoBtn = createAnyElement("button",{class: "btn btn-info"});
+     infoBtn.innerHTML = '<i class="fa fa-refresh" aria-hidden="true"></i>';
+     let delBtn = createAnyElement("button",{class: "btn btn-danger"});
+     delBtn.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+     group.appendChild(infoBtn);
+     group.appendChild(delBtn);
 
+     let td = createAnyElement ("td");
+     td.appendChild(group);
+     return td;
+}
