6d067212e0b00d767eb7436f54ee7be7478df330
KPKate <palanki.katalin@gmail.com>
2020-08-08 00:23:07 +0200

    félig törlés kész

--- IFKSP-Server/js/main.js
+++ IFKSP-Server/js/main.js
@@ -1,34 +1,35 @@
 // Get data from the server.
 function getServerData(url) {
-    let fetchOptions ={
-        metod:"GET",
-        mode:"cors",
-        cache:"no-cache"
+    let fetchOptions = {
+        metod: "GET",
+        mode: "cors",
+        cache: "no-cache"
     };
 
-   return fetch (url,fetchOptions).then(
-        response=> response.json(),
-        err=> console.error(err)
-     );
+    return fetch(url, fetchOptions).then(
+        response => response.json(),
+        err => console.error(err)
+    );
 }
 
-document.querySelector("#getDataBtn").addEventListener("click", function() {
+function startGetUsers() {
     getServerData("http://localhost:3000/users").then(
         data => fillDataTable(data, "userTable")
-        ) ;
-});
+    );
+}
+document.querySelector("#getDataBtn").addEventListener("click", startGetUsers);
 
-   //Fill table with server data,
-   function fillDataTable(data,tableID) {
-       let table = document.querySelector(`#${tableID}`);
-       if (!table) {
-           console.error(`Table "${tableID}" is not found.`);
-           return;
-       }
+//Fill table with server data,
+function fillDataTable(data, tableID) {
+    let table = document.querySelector(`#${tableID}`);
+    if (!table) {
+        console.error(`Table "${tableID}" is not found.`);
+        return;
+    }
 
-       let tBody = table.querySelector("tbody");
-       for (let row of data) {
-        let tr = createAnyElement("tr");  
+    let tBody = table.querySelector("tbody");
+    for (let row of data) {
+        let tr = createAnyElement("tr");
         for (let k in row) {
             let td = createAnyElement("td");
             td.innerHTML = row[k];
@@ -37,27 +38,50 @@ document.querySelector("#getDataBtn").addEventListener("click", function() {
         let btnGroup = createBtnGroup();
         tr.appendChild(btnGroup);
         tBody.appendChild(tr);
-       }
-   }
+    }
+}
 
-   function createAnyElement(name, attributes) {
-        let element = document.createElement(name);
-        for (let k in attributes) {
-            element.setAttribute(k,attributes[k]);
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name);
+    for (let k in attributes) {
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
+
+function createBtnGroup() {
+    let group = createAnyElement("div", { class: "btn btn-group" });
+    let infoBtn = createAnyElement("button", { class: "btn btn-info",/* onclick: "getInfo(this)"*/ });
+    infoBtn.innerHTML = '<i class="fa fa-refresh" aria-hidden="true"></i>';
+    let delBtn = createAnyElement("button", { class: "btn btn-danger", onclick: "delrow(this)" });
+    delBtn.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+
+    group.appendChild(infoBtn);
+    group.appendChild(delBtn);
+
+    let td = createAnyElement("td");
+    td.appendChild(group);
+    return td;
+}
+
+function delrow(btn) {
+    let tr = btn.parentElement.parentElement.parentElement;
+    console.log(tr);
+    let id = tr.querySelector("td:first-child").innerHTML;
+    console.log("id", id);
+
+    let fetchOptions = {
+        method: "DELETE",
+        mode: "cors",
+        cache: "no-cache"
+    };
+
+    fetch(`http://localhost:3000/users/${id}`, fetchOptions).then(
+        resp => resp.json(),
+        err => console.error(err)
+    ).then(
+        data => {
+            startGetUsers();
         }
-        return element;
-   }
-
-function createBtnGroup () {
-    let group = createAnyElement("div",{class: "btn btn-group"}); 
-     let infoBtn = createAnyElement("button",{class: "btn btn-info"});
-     infoBtn.innerHTML = '<i class="fa fa-refresh" aria-hidden="true"></i>';
-     let delBtn = createAnyElement("button",{class: "btn btn-danger"});
-     delBtn.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
-     group.appendChild(infoBtn);
-     group.appendChild(delBtn);
-
-     let td = createAnyElement ("td");
-     td.appendChild(group);
-     return td;
+    );
 }
